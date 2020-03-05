52b5e0ea1ccb125507a2d7b606f8f260b4ec8566
Bruno Cezar <cezarb348@gmail.com>
2020-03-04 22:26:09 -0300

    aula01

---
+++ aulas/basic.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+
+# nome = 'Renato'
+# idade = 26
+# altura = 1.70
+# brasileiro = True
+
+
+# print('Olá meu nome é',nome ,sobrenome) # Primeira opção
+# print('Olá meu nome é {}'.format(nome)) # Segunda opção
+# print(f'Olá meu nome é {nome}') # Terceira opção
+
+# Vendo tipos de dados
+# print(type(nome))
+# print(type(idade))
+# print(type(altura))
+# print(type(brasileiro))
+
+# Entrada básica de dados
+# nome = input('Digite seu nome: ')
+# print(nome)
+
+# Conversao de tipo de dados
+# num1 = float(input('Digite o primeiro numero: '))
+# num2 = float(input('Digite o segundo numero: '))
+# soma = num1+num2
+
+# print(str(soma))
+
+texto = 'esse é um texto'
+
+print(texto.upper())
+print(texto.lower())
+print(texto.capitalize())
+print(texto.split(' '))
---
+++ exercicios/basic_ex.py
@@ -0,0 +1,11 @@
+# 1. Crie um script com duas variaveis
+# nome e sobrenome
+# e imprima na tela 
+# a mensagem Olá meu nome sobrenome
+
+nome = 'Bruno'
+sobrenome = 'Cezar'
+
+print(f'Olá meu nome é {nome} {sobrenome}')
+print('Olá meu nome é {} {}'.format(nome, sobrenome))
+print('Olá meu nome é',nome,sobrenome)
