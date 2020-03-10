e16dcdf29c2244c98bb25a8e531d38c2abfcc8e0
Bruno Cezar <cezarb348@gmail.com>
2020-03-09 22:24:26 -0300

    Aula 4

---
+++ aulas/arquivos.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# with open('primeiro_arquivo', 'w') as f:
+#     f.seek(0)
+#     f.write('\nCurso de Python')
+#     f.close()
+
+with open('primeiro_arquivo', 'r') as f:
+     print(f.read())
